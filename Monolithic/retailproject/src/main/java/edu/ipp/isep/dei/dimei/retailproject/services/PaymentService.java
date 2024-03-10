54e0bd86b088d7f541431b7e538b480738f7d22e
João Campelo <joaocampelo3@gmail.com>
2024-03-10 01:00:30 +0000

    Create Payment Repository and Service

---
+++ Monolithic/retailproject/src/main/java/edu/ipp/isep/dei/dimei/retailproject/services/PaymentService.java
@@ -0,0 +1,35 @@
+package edu.ipp.isep.dei.dimei.retailproject.services;
+
+import edu.ipp.isep.dei.dimei.retailproject.common.dto.gets.PaymentDTO;
+import edu.ipp.isep.dei.dimei.retailproject.domain.model.Payment;
+import edu.ipp.isep.dei.dimei.retailproject.exceptions.NotFoundException;
+import edu.ipp.isep.dei.dimei.retailproject.repositories.PaymentRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public class PaymentService {
+    private final PaymentRepository paymentRepository;
+
+    public Payment createPayment(PaymentDTO paymentDTO) throws NotFoundException {
+        Payment payment = paymentDTO.dtoToEntity();
+
+        if (!existsPaymentByStreetAndZipCodeAndCityAndCountry(payment)) {
+            this.paymentRepository.save(payment);
+        }
+
+        return getPayment(payment);
+    }
+
+    private boolean existsPaymentByStreetAndZipCodeAndCityAndCountry(Payment payment) {
+        return this.paymentRepository.existsPaymentByAmountAndPaymentDateTimeAndPaymentMethod(payment.getAmount(), payment.getPaymentDateTime(), payment.getPaymentMethod());
+    }
+
+    public Payment getPayment(Payment payment) throws NotFoundException {
+        return this.paymentRepository.findPaymentByAmountAndPaymentDateTimeAndPaymentMethod(payment.getAmount(), payment.getPaymentDateTime(), payment.getPaymentMethod())
+                .orElseThrow(() -> new NotFoundException("Payment not found."));
+    }
+}
