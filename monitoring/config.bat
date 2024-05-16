9a2759669de549433408cd93eb2b398e1bececf9
João Campelo <joaocampelo3@gmail.com>
2024-05-16 23:29:04 +0100

    Add Grafana and Kepler configuration scripts

--- monitoring/config.bat
+++ monitoring/config.bat
@@ -5,6 +5,3 @@ kubectl create namespace kepler
 
 Grafana\config.bat
 Kepler\config.bat
-
-kubectl port-forward --address localhost -n kepler service/kepler-exporter 9102:9102 &
-kubectl port-forward --address localhost -n monitoring service/prometheus-k8s 9091:9091 &
