6de12f667b5c5c1caec88c101dbf43e57183003b
João Campelo <joaocampelo3@gmail.com>
2024-05-19 22:23:41 +0100

    Add Grafana, Prometheus and Kepler configuration files

--- monitoring/kind_config.sh
+++ monitoring/kind_config.sh
@@ -1,5 +1,3 @@
-kubectl delete namespace monitoring --ignore-not-found
-kubectl delete namespace kepler --ignore-not-found
-sudo kind delete kepler-cluster
+sudo kind delete cluster -n kepler-cluster
 export CLUSTER_NAME="kepler-cluster"
 sudo kind create cluster --name=$CLUSTER_NAME --config=./local-cluster-config.yaml
