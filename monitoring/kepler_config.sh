9de1a8f5e29532010dd8622e64fb610098c6b7a9
João Campelo <joaocampelo3@gmail.com>
2025-02-11 23:26:09 +0100

    Fix Jenkins Phase 2 pipeline

--- monitoring/kepler_config.sh
+++ monitoring/kepler_config.sh
@@ -1,6 +1,6 @@
 make build-manifest OPTS="PROMETHEUS_DEPLOY"
-sleep 7
 kubectl apply -f _output/generated-manifest/deployment.yaml
+sleep 7
 kubectl port-forward --address localhost -n kepler service/kepler-exporter 9102:9102 &
 kubectl port-forward --address localhost -n monitoring service/prometheus-k8s 9090:9090 &
 kubectl port-forward --address localhost -n monitoring service/grafana 3000:3000 &
