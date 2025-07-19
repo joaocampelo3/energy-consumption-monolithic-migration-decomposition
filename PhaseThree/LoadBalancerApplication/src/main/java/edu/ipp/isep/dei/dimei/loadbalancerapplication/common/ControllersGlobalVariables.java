861b9db5e5b4d39e8ca3543a298690446eb0995f
João Campelo <joaocampelo3@gmail.com>
2025-07-19 17:47:19 +0200

    Phase 3 - Fix API Gateway

--- PhaseThree/LoadBalancerApplication/src/main/java/edu/ipp/isep/dei/dimei/loadbalancerapplication/common/ControllersGlobalVariables.java
+++ PhaseThree/LoadBalancerApplication/src/main/java/edu/ipp/isep/dei/dimei/loadbalancerapplication/common/ControllersGlobalVariables.java
@@ -4,11 +4,12 @@ public class ControllersGlobalVariables {
     public static final String MONOLITH_URL = "http://RETAILPROJECTMONOLITH";
     public static final String USERS_URL = "http://USERMICROSERVICEAPP";
     public static final String ITEMS_URL = "http://ITEMMICROSERVICEAPP";
+    public static final String ORDERS_URL = "http://ORDERMICROSERVICEAPP";
     public static final String CATEGORY_URL = ITEMS_URL + "/categories";
     public static final String ITEM_URL = ITEMS_URL + "/items";
     public static final String MERCHANT_URL = MONOLITH_URL + "/merchants";
     public static final String MERCHANT_ORDER_URL = MONOLITH_URL + "/merchantorders";
-    public static final String ORDER_URL = MONOLITH_URL + "/orders";
+    public static final String ORDER_URL = ORDERS_URL + "/orders";
     public static final String SHIPPING_ORDER_URL = MONOLITH_URL + "/shippingorders";
 
     private ControllersGlobalVariables() {
