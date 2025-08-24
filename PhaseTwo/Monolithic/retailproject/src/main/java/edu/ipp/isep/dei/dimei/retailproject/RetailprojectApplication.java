4d7fd397cfd6ac004228fbe0d9c9d038f61b7eb0
João Campelo <joaocampelo3@gmail.com>
2025-08-24 22:09:07 +0200

    Fix start data Monolithic - Phase2

--- PhaseTwo/Monolithic/retailproject/src/main/java/edu/ipp/isep/dei/dimei/retailproject/RetailprojectApplication.java
+++ PhaseTwo/Monolithic/retailproject/src/main/java/edu/ipp/isep/dei/dimei/retailproject/RetailprojectApplication.java
@@ -1,6 +1,8 @@
 package edu.ipp.isep.dei.dimei.retailproject;
 
+import edu.ipp.isep.dei.dimei.retailproject.domain.model.Item;
 import edu.ipp.isep.dei.dimei.retailproject.domain.model.Merchant;
+import edu.ipp.isep.dei.dimei.retailproject.repositories.ItemRepository;
 import edu.ipp.isep.dei.dimei.retailproject.repositories.MerchantRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -19,7 +21,7 @@ public class RetailprojectApplication {
     }
 
     @Bean
-    public CommandLineRunner demoData(MerchantRepository merchantRepository) {
+    public CommandLineRunner demoData(MerchantRepository merchantRepository, ItemRepository itemRepository) {
         return args -> {
             Merchant merchant = null;
 
@@ -28,6 +30,13 @@ public class RetailprojectApplication {
                 merchant = new Merchant("Merchant Dummy", "merchant@gmail.com", 2);
                 merchantRepository.save(merchant);
                 merchant.setId(1);
+            } else {
+                merchant = merchantCheck;
+            }
+
+            if (merchantCheck != null) {
+                itemRepository.findById(1).orElse(itemRepository.save(new Item("Item 1", "ABC-12345-S-BL", "Item 1 description", 8, 20000, merchant)));
+                itemRepository.findById(2).orElse(itemRepository.save(new Item("Item 2", "ABC-12345-XL-BL", "Item 2 description", 5, 30000, merchant)));
             }
         };
     }
